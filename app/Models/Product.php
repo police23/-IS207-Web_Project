0d924e60210b0d9db5b248f59bd17832a62168cf
tdv1610 <22521681@gm.uit.edu.vn>
2024-11-19 13:28:58 +0700

    hoàn thành 95% giao diện admin, tạo cmv cho 3 bảng users, products,brand

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $table = 'tbl_product';
+    protected $table = 'products';
 
     protected $primaryKey = 'product_id';
 
